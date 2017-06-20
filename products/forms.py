1d3356fc338c538f1561e7e4bf9e375054cbb445
noname <noname@MacBook-Pro-noname.local>
2017-06-20 23:38:41 +0300

    added widget

--- products/forms.py
+++ products/forms.py
@@ -1,6 +1,28 @@
 from django import forms
-
+PUBLISH_CHOICES = (
+    #('', ""),
+    ('publish', 'Publish'),
+    ('draft', 'Draft'),
+)
 class ProductAddForm(forms.Form):
     title = forms.CharField()
-    description = forms.CharField()
+    description = forms.CharField(widget=forms.Textarea)
     price = forms.DecimalField()
+    publish = forms.ChoiceField(widget=forms.RadioSelect, choices=PUBLISH_CHOICES, required=False)
+
+
+    def clean_price(self):
+        price = self.cleaned_data.get("price")
+        if price <= 1.00:
+            raise forms.ValidationError("Price must be greater than $1.00")
+        elif price >= 99.99:
+            raise forms.ValidationError("Price must be less than $100.00")
+        else:
+            return price
+
+    def clean_title(self):
+        title = self.cleaned_data.get("title")
+        if len(title) > 3:
+            return title
+        else:
+            raise forms.ValidationError("title must be greater than 3 char long")
