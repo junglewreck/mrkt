f6d00a1fb517e10ab1c6b7ed9c8c922b25d61515
junglewreck <jesuschristlovesthc@gmail.com>
2017-07-18 00:39:55 +0200

    DetailView Class Slug Fix

--- products/views.py
+++ products/views.py
@@ -10,7 +10,20 @@ from .forms import ProductAddForm, ProductModelForm
 
 class ProductDetailView(DetailView):
 	model = Product
+	def get_object(self, *args, **kwargs):
+		slug = self.kwargs.get("slug")
+		ModelClass = self.model
+		if slug is not None:
+			try:
+				obj = get_object_or_404(ModelClass, slug=slug)
+			except ModelClass.MultipleObjectsReturned:
+				obj = ModelClass.objects.filter(slug=slug).order_by("-title").first()
+			else:
+				obj = super(ProductDetailView, self).get_object(*args, **kwargs)
+			return obj
 
+		obj = super(ProductDetailView, self).get_object(*args, **kwargs)
+		return obj
 class ProductListView(ListView):
 	model = Product
 	# template_name = "list_view.html"
